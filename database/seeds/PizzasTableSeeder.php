ae3f176a637c914269a6935fbc29d3e752f33369
ArminCoder <armincoding@gmail.com>
2020-09-23 20:35:26 +0200

    small change inside pizza seeder

--- database/seeds/PizzasTableSeeder.php
+++ database/seeds/PizzasTableSeeder.php
@@ -14,52 +14,52 @@ class PizzasTableSeeder extends Seeder
     {
         DB::table('pizzas')->insert([
             [
-                'name' => 'Margharita',
+                'name' => 'Margherita',
                 'image' => '/images/margarita.png',
                 'price' => '6',
-                'description' => "Pizza Margherita (more commonly known in English as Margherita pizza) is a typical Neapolitan pizza, made with San Marzano tomatoes, mozzarella cheese, fresh basil, salt and extra-virgin olive oil. Traditionally, it is made with fior di latte (cow's milk mozzarella) rather than buffalo mozzarella."
+                'description' => "I absolutely love Italian food, So pizza - like a margherita pizza, penne arbiota, a glass of wine. That's like my dream. ~Julianne Hough"
             ],
             [
-                'name' => 'Capricosa',
-                'image' => '/images/margarita.png',
+                'name' => 'Capriciossa',
+                'image' => '/images/capriciossa.png',
                 'price' => '8',
-                'description' => 'Lorem ipsum'
+                'description' => "You better cut the capriciossa in four pieces because I'm not hungry enough to eat six. ~Yogi Berra"
             ],
             [
                 'name' => 'Funghi',
-                'image' => '/images/margarita.png',
+                'image' => '/images/funghi.png',
                 'price' => '7',
-                'description' => 'Lorem ipsum'
+                'description' => "I don't always take off my make-up before bed, and I like funghi at two in the morning. ~Katie Holmes"
             ],
             [
                 'name' => 'Vegetariana',
-                'image' => '/images/margarita.png',
+                'image' => '/images/vegeteriana.png',
                 'price' => '8',
-                'description' => 'Lorem ipsum'
+                'description' => "I can't remember a Friday when I was younger when I wasn't eating a vegeteriana, flirting with the barman. ~Florence Pugh"
             ],
             [
                 'name' => 'Mexicana',
-                'image' => '/images/margarita.png',
+                'image' => '/images/mexicana.png',
                 'price' => '10',
-                'description' => 'Lorem ipsum'
+                'description' => "I love mexicana. I want to marry it, but it would just be to eat her family at the wedding. ~Mike Birbiglia"
             ],
             [
                 'name' => 'Tonno',
-                'image' => '/images/margarita.png',
+                'image' => '/images/tonno.png',
                 'price' => '9',
-                'description' => 'Lorem ipsum'
+                'description' => "I grew up with a Mediterranean diet, so I like clean, simple food. But if there's tonno on a menu, I always end up ordering it. I can't resist it. ~Sara Sampaio"
             ],
             [
                 'name' => 'Pepperoni',
-                'image' => '/images/margarita.png',
+                'image' => '/images/pepperoni.png',
                 'price' => '8',
-                'description' => 'Lorem ipsum'
+                'description' => "It doesn't even matter if I don't win a self-gold after every meet, I have pizza. Peperoni pizza! ~Simone Biles"
             ],
             [
                 'name' => 'Quattro formaggi',
-                'image' => '/images/margarita.png',
+                'image' => '/images/guattro_formaggi.png',
                 'price' => '10',
-                'description' => 'Lorem ipsum'
+                'description' => "I love Italian food; you can't go wrong with Rosebud, and of course, I like quattro formaggi pizza. ~Shanola Hampton"
             ]
          ]);
     }
